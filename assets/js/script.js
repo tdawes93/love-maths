a1b63801aed9297cfb7b29dd4b752b20dd14dbe8
tdawes93 <tdawes136@googlemail.com>
2021-12-24 17:13:38 +0000

    Add changes to runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,27 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked submit!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition"); 
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+    }
 }
 
 function checkAnswer() {
